a6ed020492cea0d6576951ed2d0393a80581ee7e
Aphrodis <niyomurengeziaphrodis@gmail.com>
2019-08-31 09:16:48 +0200

    Change a user to a mentor

--- api/routes/userRouter.js
+++ api/routes/userRouter.js
@@ -9,4 +9,9 @@ router.post('/POST/auth/signup', db.signUp);
 
 //sign in
 router.post('/POST/auth/signin', db.login);
+
+//change a user to amentor
+router.patch('/PATCH/user/:userId', checkToken, db.changeToMentor);
+
+
 export default router;
